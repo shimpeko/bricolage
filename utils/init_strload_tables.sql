af38127170ff5274270a3a3e2aff83def1e33661
Shimpei Kodama <shimpei-kodama@cookpad.com>
2016-06-17 18:27:33 +0900

    Update util script & add view for stats

--- utils/init_strload_tables.sql
+++ utils/init_strload_tables.sql
@@ -1,14 +1,13 @@
 drop table if exists strload_job_params;
-drop table if exists strload_objects;
-drop table if exists strload_tables;
+drop table if exists strload_objects cascade;
 drop table if exists strload_task_objects;
-drop table if exists strload_tasks;
-drop table if exists strload_jobs;
+drop table if exists strload_tasks cascade;
+drop table if exists strload_jobs cascade;
 
 \i schema/strload_job_params.ct
 \i schema/strload_objects.ct
-\i schema/strload_tables.ct
 \i schema/strload_task_objects.ct
 \i schema/strload_tasks.ct
 \i schema/strload_jobs.ct
+\i schema/strload_stats.cv
 
